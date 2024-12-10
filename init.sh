29c7c3e90f4f475cb8d803aa1967e678177563a3
Kohei Noda <ponserviceregist@gmail.com>
2024-12-10 10:30:53 +0900

    init.sh: add non-guest user

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ set -eu
 
 rabbitmqadmin declare queue name=test
 rabbitmqadmin declare binding source=amq.topic destination=test routing_key=test.topic
+
+# add non-guest user
+MQTT_USER="mqtt-test"
+rabbitmqctl add_user $MQTT_USER $MQTT_USER
+rabbitmqctl set_permission -p "/" $MQTT_USER ".*" ".*" ".*"
+rabbitmqctl set_user_tags management
