7024aad91c753ed391d8d9a4b6aebe4b1acea32f
Kohei Noda <k.noda@saver.jp>
2024-12-19 18:36:12 +0900

    add DockerfileTLS

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [[ -z "$1" ]]; then
+	echo "build no-tls Dockerfile"
+	DOCKERFILE="Dockerfile"
+elif [[ ! -d "$1" ]]; then
+	echo "$1 not found"
+	echo "build no-tls Dockerfile"
+	DOCKERFILE="Dockerfile"
+else
+	echo "build tls Dockerfile"
+	DOCKERFILE="DockerfileSSL"
+	TLS="$1"
+fi
+
+docker compose build --build-arg TLS=$TLS
