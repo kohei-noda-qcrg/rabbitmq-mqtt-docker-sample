261fc20095b6b6267e29b1b46d78997381ad3cfa
Kohei Noda <k.noda@saver.jp>
2024-12-24 12:49:47 +0900

    build.sh: use .env file to pass environment variables to compose.yml

--- build.sh
+++ build.sh
@@ -2,16 +2,17 @@
 
 set -eo pipefail
 
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
 if [[ -z "$1" ]]; then
 	echo "build no-tls Dockerfile"
-	DOCKERFILE="Dockerfile"
+	echo -e "DOCKERFILE=Dockerfile\nPORTMAP=1883:1883" > $script_dir/.env
 elif [[ ! -d "$1" ]]; then
 	echo "$1 not found. Please provide a valid path to the TLS directory"
 	exit 1
 else
 	echo "build tls Dockerfile"
-	DOCKERFILE="DockerfileSSL"
-	TLS="$1"
+	echo -e "DOCKERFILE=DockerfileSSL\nPORTMAP=8883:8883\nTLS=$1" > $script_dir/.env
 fi
 
-docker compose build --build-arg TLS=$TLS --no-cache
+docker compose build --no-cache
