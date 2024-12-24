af546341ed08dde1a78edf961c75a907250b45a2
Kohei Noda <ponserviceregist@gmail.com>
2024-12-24 12:54:59 +0900

    build.sh: write .env file to the current directory

--- build.sh
+++ build.sh
@@ -2,17 +2,15 @@
 
 set -eo pipefail
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
 if [[ -z "$1" ]]; then
 	echo "build no-tls Dockerfile"
-	echo -e "DOCKERFILE=Dockerfile\nPORTMAP=1883:1883" > $script_dir/.env
+	echo -e "DOCKERFILE=Dockerfile\nPORTMAP=1883:1883" > .env
 elif [[ ! -d "$1" ]]; then
 	echo "$1 not found. Please provide a valid path to the certs directory"
 	exit 1
 else
 	echo "build tls Dockerfile"
-	echo -e "DOCKERFILE=DockerfileSSL\nPORTMAP=8883:8883\nTLS=$1" > $script_dir/.env
+	echo -e "DOCKERFILE=DockerfileSSL\nPORTMAP=8883:8883\nTLS=$1" > .env
 fi
 
 docker compose build --no-cache
