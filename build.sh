07f0c9d61495b853b22cf33b960ab13c4933fd84
Kohei Noda <k.noda@saver.jp>
2024-12-24 12:53:21 +0900

    build.sh: s/TLS/certs

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if [[ -z "$1" ]]; then
 	echo "build no-tls Dockerfile"
 	echo -e "DOCKERFILE=Dockerfile\nPORTMAP=1883:1883" > $script_dir/.env
 elif [[ ! -d "$1" ]]; then
-	echo "$1 not found. Please provide a valid path to the TLS directory"
+	echo "$1 not found. Please provide a valid path to the certs directory"
 	exit 1
 else
 	echo "build tls Dockerfile"
