a369308824dda51b458ec169737783ee6ac7c657
Kohei Noda <k.noda@saver.jp>
2025-01-10 11:49:31 +0900

    build.sh: add blank TLS variable to .env file

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 if [[ -z "$1" ]]; then
 	echo "build no-tls Dockerfile"
-	echo -e "DOCKERFILE=Dockerfile\nPORTMAP=1883:1883" > .env
+	echo -e "DOCKERFILE=Dockerfile\nPORTMAP=1883:1883\nTLS=" > .env
 elif [[ ! -d "$1" ]]; then
 	echo "$1 not found. Please provide a valid path to the certs directory"
 	exit 1
